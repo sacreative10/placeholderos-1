e6e030b303939e71ae553058edab313453d45655
Sohaib <sacreative10@gmail.com>
2021-02-16 22:26:18 +0000

    C infastructure changes

--- src/boot/C/kernel.c
+++ src/boot/C/kernel.c
@@ -1,111 +1,17 @@
-#pragma once
-
-#include <stdint.h>
-#include <stddef.h>
-
-enum {
-    PRINT_COLOR_BLACK = 0,
-	PRINT_COLOR_BLUE = 1,
-	PRINT_COLOR_GREEN = 2,
-	PRINT_COLOR_CYAN = 3,
-	PRINT_COLOR_RED = 4,
-	PRINT_COLOR_MAGENTA = 5,
-	PRINT_COLOR_BROWN = 6,
-	PRINT_COLOR_LIGHT_GRAY = 7,
-	PRINT_COLOR_DARK_GRAY = 8,
-	PRINT_COLOR_LIGHT_BLUE = 9,
-	PRINT_COLOR_LIGHT_GREEN = 10,
-	PRINT_COLOR_LIGHT_CYAN = 11,
-	PRINT_COLOR_LIGHT_RED = 12,
-	PRINT_COLOR_PINK = 13,
-	PRINT_COLOR_YELLOW = 14,
-	PRINT_COLOR_WHITE = 15,
-};
-const static size_t NUM_COLS = 80;
-const static size_t NUM_ROWS = 25;
-
-struct Char {
-    uint8_t character;
-    uint8_t color;
-};
-
-struct Char* buffer = (struct Char*) 0xb8000;
-size_t col = 0;
-size_t row = 0;
-uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
-
-void clear_row(size_t row) {
-    struct Char empty = (struct Char) {
-        character: ' ',
-        color: color,
-    };
-
-    for (size_t col = 0; col < NUM_COLS; col++) {
-        buffer[col + NUM_COLS * row] = empty;
-    }
-}
-
-void print_clear() {
-    for (size_t i = 0; i < NUM_ROWS; i++) {
-        clear_row(i);
-    }
-}
-
-void print_newline() {
-    col = 0;
-
-    if (row < NUM_ROWS - 1) {
-        row++;
-        return;
-    }
-
-    for (size_t row = 1; row < NUM_ROWS; row++) {
-        for (size_t col = 0; col < NUM_COLS; col++) {
-            struct Char character = buffer[col + NUM_COLS * row];
-            buffer[col + NUM_COLS * (row - 1)] = character;
-        }
-    }
-
-    clear_row(NUM_COLS - 1);
-}
-
-void print_char(char character) {
-    if (character == '\n') {
-        print_newline();
-        return;
-    }
-
-    if (col > NUM_COLS) {
-        print_newline();
-    }
-
-    buffer[col + NUM_COLS * row] = (struct Char) {
-        character: (uint8_t) character,
-        color: color,
-    };
-
-    col++;
-}
-
-void print_str(char* str) {
-    for (size_t i = 0; 1; i++) {
-        char character = (uint8_t) str[i];
-
-        if (character == '\0') {
-            return;
-        }
-
-        print_char(character);
-    }
-}
-
-void print_set_color(uint8_t foreground, uint8_t background) {
-    color = foreground + (background << 4);
-}
-
+#include "print.h"
 
 void kernel_main() {
     print_clear();
-    print_set_color(PRINT_COLOR_YELLOW, PRINT_COLOR_BLACK);
-    print_str("This worked ok now we go bye bye");
+    print_set_color(PRINT_COLOR_RED, PRINT_COLOR_BLACK);
+    print_str("Welcome to JunaidOS\n");
+    print_str("Built By Sohaib Alam\n");
+    print_str("#######\n");
+    print_str("#   # #\n");
+    print_str("# # # #      JunaidOS\n");
+    print_str("#  # ##        v.0.001\n");
+    print_str("#    ##\n");
+    print_str("#######\n");
+    print_str("Have a nice time\n");
+
+
 }
---
+++ src/boot/C/print.h
@@ -0,0 +1,104 @@
+#pragma once
+
+#include <stdint.h>
+#include <stddef.h>
+
+enum {
+    PRINT_COLOR_BLACK = 0,
+	PRINT_COLOR_BLUE = 1,
+	PRINT_COLOR_GREEN = 2,
+	PRINT_COLOR_CYAN = 3,
+	PRINT_COLOR_RED = 4,
+	PRINT_COLOR_MAGENTA = 5,
+	PRINT_COLOR_BROWN = 6,
+	PRINT_COLOR_LIGHT_GRAY = 7,
+	PRINT_COLOR_DARK_GRAY = 8,
+	PRINT_COLOR_LIGHT_BLUE = 9,
+	PRINT_COLOR_LIGHT_GREEN = 10,
+	PRINT_COLOR_LIGHT_CYAN = 11,
+	PRINT_COLOR_LIGHT_RED = 12,
+	PRINT_COLOR_PINK = 13,
+	PRINT_COLOR_YELLOW = 14,
+	PRINT_COLOR_WHITE = 15,
+};
+const static size_t NUM_COLS = 80;
+const static size_t NUM_ROWS = 25;
+
+struct Char {
+    uint8_t character;
+    uint8_t color;
+};
+
+struct Char* buffer = (struct Char*) 0xb8000;
+size_t col = 0;
+size_t row = 0;
+uint8_t color = PRINT_COLOR_WHITE | PRINT_COLOR_BLACK << 4;
+
+void clear_row(size_t row) {
+    struct Char empty = (struct Char) {
+        character: ' ',
+        color: color,
+    };
+
+    for (size_t col = 0; col < NUM_COLS; col++) {
+        buffer[col + NUM_COLS * row] = empty;
+    }
+}
+
+void print_clear() {
+    for (size_t i = 0; i < NUM_ROWS; i++) {
+        clear_row(i);
+    }
+}
+
+void print_newline() {
+    col = 0;
+
+    if (row < NUM_ROWS - 1) {
+        row++;
+        return;
+    }
+
+    for (size_t row = 1; row < NUM_ROWS; row++) {
+        for (size_t col = 0; col < NUM_COLS; col++) {
+            struct Char character = buffer[col + NUM_COLS * row];
+            buffer[col + NUM_COLS * (row - 1)] = character;
+        }
+    }
+
+    clear_row(NUM_COLS - 1);
+}
+
+void print_char(char character) {
+    if (character == '\n') {
+        print_newline();
+        return;
+    }
+
+    if (col > NUM_COLS) {
+        print_newline();
+    }
+
+    buffer[col + NUM_COLS * row] = (struct Char) {
+        character: (uint8_t) character,
+        color: color,
+    };
+
+    col++;
+}
+
+void print_str(char* str) {
+    for (size_t i = 0; 1; i++) {
+        char character = (uint8_t) str[i];
+
+        if (character == '\0') {
+            return;
+        }
+
+        print_char(character);
+    }
+}
+
+void print_set_color(uint8_t foreground, uint8_t background) {
+    color = foreground + (background << 4);
+}
