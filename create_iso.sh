7b451385c0ca2475d639490216a50cd1ae40c4a2
Sohaib <sacreative10@gmail.com>
2021-02-17 22:04:15 +0000

    update create_iso.sh

--- create_iso.sh
+++ create_iso.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd src/boot
-sh ./clean.sh
+./clean.sh
 mkdir outputdir
 ~/opt/cross/i686-elf/bin/as asm/boot.s -o outputdir/boot.o
 ~/opt/cross/bin/i686-elf-gcc -c C/kernel.c -o outputdir/kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
